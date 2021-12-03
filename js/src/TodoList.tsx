b2194f2d3f718f16f61b24286d3668413545a9f5
Tom Burns <tom@burns.ca>
2021-12-02 23:44:47 -0500

    enable packaging SCSS into the JS bundle at build time

---
+++ js/src/TodoList.tsx
@@ -0,0 +1,10 @@
+import { Component, h } from "preact";
+import styles from "./TodoList.scss";
+
+export class TodoList extends Component {
+    render() {
+        return <div style={styles.container}>
+
+        </div>
+    }
+}
